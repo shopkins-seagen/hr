bb40e5a6d7225bf4f70edce1f9f332ec45030895
Shawn hopkins <shawn.hopkins1@gmail.com>
2024-11-16 13:45:42 -0800

    updated app port

--- src/assignment06/app.py
+++ src/assignment06/app.py
@@ -154,6 +154,6 @@ def emp_to_dict(e):
 
 
 if __name__ == '__main__':
-    app.run()
-    # port = int(os.environ.get("PORT", 6738))
-    # app.run(host='0.0.0.0', port=port)
+    # app.run()
+    port = int(os.environ.get("PORT", 6738))
+    app.run(host='0.0.0.0', port=port)
