dc3eb7a73489a95f0b55cd9ddf693e6e6ea9d038
Shawn hopkins <shawn.hopkins1@gmail.com>
2024-11-16 13:36:58 -0800

    added code for cdn for js and css

--- src/assignment06/init_db.py
+++ src/assignment06/init_db.py
@@ -5,5 +5,6 @@ from models import db,Employee,Review,upload_employees
 db.connect()
 db.drop_tables([Employee,Review])
 db.create_tables([Employee,Review])
+upload_employees('static/uploads/MOCK_DATA_3.csv')
 
 
